bb0eb0315a064e95f4b414799e9de30c60600df5
BartoszHesse <bartoszh@onet.eu>
2021-12-19 18:19:31 +0100

    Dodanie metody GetDistance, panelu i pliku txt

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,39 @@
 package com.company;
 
+import javax.swing.*;
+import java.awt.*;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 public class Main {
+    public static double distance = 0;
+
+    public static void ReadFromFile() throws FileNotFoundException {
+        File file = new File("Plansza.txt");
+        Scanner in = new Scanner(file);
+
+    }
+
+    public static void GetDistance(int x1, int y1, int x2, int y2){
+        double a = ((x2 - x1) * (x2 - x1)) + ((y2 - y1) * (y2 - y1));
+        distance = Math.sqrt(a);
+    }
 
     public static void main(String[] args) {
-	
+        /*GetDistance(10, 19, 15, 7);
+        System.out.println(distance); */
+        JFrame gra = new JFrame();
+        Panel panel = new Panel();
+        gra.add(panel);
+        //gra.setExtendedState(JFrame.MAXIMIZED_BOTH);
+        Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+        gra.setSize(screenSize.width, screenSize.height);
+        //gra.setUndecorated(true);
+        gra.setResizable(false);
+        gra.setTitle("Pac-Man");
+        gra.setLocationRelativeTo(null);
+        gra.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        gra.setVisible(true);
     }
 }
