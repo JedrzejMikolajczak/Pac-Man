abd2c47a39c59e6add34f86c7d65f2bb3fcddbf6
JedrzejMikolajczak <84733353+JedrzejMikolajczak@users.noreply.github.com>
2021-12-19 19:22:02 +0100

    klasa duszek

---
+++ src/com/company/Duszek.java
@@ -0,0 +1,91 @@
+package com.company;
+
+enum JakiDuszek{
+    NIEBIESKI,ROZOWY,POMARANCZOWY,CZERWONY;
+}
+enum Stan{
+    CHASE,FRIGHTENED,EATEN,SCUTTER;
+}
+public class Duszek {
+    JakiDuszek jakiDuszek;
+    int PozXduszka;
+    int PozYduszka;
+    int PozXPixelduszka;
+    int PozYPixelduszka;
+    int celX;
+    int celY;
+    Main.Kierunek kierunek;
+    Stan stan;
+
+    public JakiDuszek getJakiDuszek() {
+        return jakiDuszek;
+    }
+
+    public void setJakiDuszek(JakiDuszek jakiDuszek) {
+        this.jakiDuszek = jakiDuszek;
+    }
+
+    public int getPozXduszka() {
+        return PozXduszka;
+    }
+
+    public void setPozXduszka(int pozXduszka) {
+        PozXduszka = pozXduszka;
+    }
+
+    public int getPozYduszka() {
+        return PozYduszka;
+    }
+
+    public void setPozYduszka(int pozYduszka) {
+        PozYduszka = pozYduszka;
+    }
+
+    public int getPozXPixelduszka() {
+        return PozXPixelduszka;
+    }
+
+    public void setPozXPixelduszka(int pozXPixelduszka) {
+        PozXPixelduszka = pozXPixelduszka;
+    }
+
+    public int getPozYPixelduszka() {
+        return PozYPixelduszka;
+    }
+
+    public void setPozYPixelduszka(int pozYPixelduszka) {
+        PozYPixelduszka = pozYPixelduszka;
+    }
+
+    public int getCelX() {
+        return celX;
+    }
+
+    public void setCelX(int celX) {
+        this.celX = celX;
+    }
+
+    public int getCelY() {
+        return celY;
+    }
+
+    public void setCelY(int celY) {
+        this.celY = celY;
+    }
+
+    public Main.Kierunek getKierunek() {
+        return kierunek;
+    }
+
+    public void setKierunek(Main.Kierunek kierunek) {
+        this.kierunek = kierunek;
+    }
+
+    public Stan getStan() {
+        return stan;
+    }
+
+    public void setStan(Stan stan) {
+        this.stan = stan;
+    }
+}
